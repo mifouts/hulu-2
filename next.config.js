22b48effbecb79baa1d1c75bfb4ab254c8034c66
mifouts <fouts.mitch15@gmail.com>
2022-12-08 14:35:22 -0500

    allowing domains for images

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = {
+  images: {
+    domains: ["image.tmdb.org", "links.papareact.com"],
+  },
+};
