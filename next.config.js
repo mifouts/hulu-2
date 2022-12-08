5734532d0b2aaef9301b17ec8190b5ac2395e7cf
mifouts <fouts.mitch15@gmail.com>
2022-12-08 14:36:10 -0500

    editing domains for images

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-};
-
-module.exports = nextConfig;
-
 module.exports = {
   images: {
     domains: ["image.tmdb.org", "links.papareact.com"],
