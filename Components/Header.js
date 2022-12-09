176c396afa517f5e42dd9c1542f69b4175206b96
mifouts <fouts.mitch15@gmail.com>
2022-12-09 16:23:23 -0500

    styling

--- Components/Header.js
+++ Components/Header.js
@@ -11,7 +11,7 @@ import HeaderItem from "../Components/HeaderItem";
 
 function Header() {
   return (
-    <header className="">
+    <header className="flex flex-col sm:flex-row m-5">
       <div className="flex flex-grow justify-evenly max-w-2xl">
         <HeaderItem title="HOME" Icon={HomeIcon} />
         <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
