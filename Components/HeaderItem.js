d7ce74593066183eef3d882f149f6a6c54c56678
mifouts <fouts.mitch15@gmail.com>
2022-12-09 16:13:25 -0500

    styling header

--- Components/HeaderItem.js
+++ Components/HeaderItem.js
@@ -1,6 +1,6 @@
 function HeaderItem({ Icon, title }) {
   return (
-    <div className="flex flex-col group">
+    <div className="flex flex-col items-center cursor-pointer group">
       <Icon className="h-8 mb-1" />
       <p className="opacity-0 group-hover:opacity-100 tracking-widest">
         {title}
