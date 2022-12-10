91e3d3c58f5917a2f66887c862a04f2c337e3b40
mifouts <fouts.mitch15@gmail.com>
2022-12-10 15:23:36 -0500

    starting the nav bar and api call

---
+++ Components/Nav.js
@@ -0,0 +1,9 @@
+function Nav() {
+  return (
+    <div>
+      <h1>this is the nav bar</h1>
+    </div>
+  );
+}
+
+export default Nav;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Header from "../Components/Header"
-
+import Nav from "../Components/Nav"
 
 export default function Home() {
   return (
@@ -12,6 +12,7 @@ export default function Home() {
       </Head>
 
       <Header />
+      <Nav />
       
     </div>
   )
