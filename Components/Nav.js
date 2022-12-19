3c5fcc2ab7d92f3d6508b09239cb993390f05442
mifouts <fouts.mitch15@gmail.com>
2022-12-19 16:17:14 -0500

    styling

--- Components/Nav.js
+++ Components/Nav.js
@@ -6,7 +6,7 @@ function Nav() {
       {Object.entries(requests).map(([key, { title, url }]) => (
         <h2
           key={key}
-          className="cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500"
+          className="last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500"
         >
           {title}
         </h2>
