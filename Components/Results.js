ca1ee0d68b7f216f6a1ca538a2e731c83c5862cf
mifouts <fouts.mitch15@gmail.com>
2022-12-23 21:31:50 -0500

    starting thumbnail component

--- Components/Results.js
+++ Components/Results.js
@@ -1,7 +1,14 @@
 import React from "react";
+import Thumbnail from "../Components/Thumbnail";
 
-function Results() {
-  return <div>this is the results section</div>;
+function Results({ movies }) {
+  return (
+    <div>
+      {movies.map((movie) => (
+        <Thumbnail key={movie.id} movie={movie} />
+      ))}
+    </div>
+  );
 }
 
 export default Results;
---
+++ Components/Thumbnail.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Thumbnail({ movie }) {
+  return (
+    <div>
+      <h1>thumbnail</h1>
+    </div>
+  );
+}
+
+export default Thumbnail;
