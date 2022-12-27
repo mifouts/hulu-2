f9a9bc414757d13511e9fd2aa2831830addd4a33
mifouts <fouts.mitch15@gmail.com>
2022-12-27 14:12:03 -0500

    thumbnail

--- Components/Thumbnail.js
+++ Components/Thumbnail.js
@@ -1,9 +1,20 @@
 import React from "react";
+import Image from "next/image";
 
 function Thumbnail({ movie }) {
+  const BASE_URL = "https://image.tmdb.org/t/p/original/";
   return (
     <div>
-      <h1>this is the thumbnail</h1>
+      <Image
+        layout="responsive"
+        height={1080}
+        width={1920}
+        alt=""
+        src={
+          `${BASE_URL}${movie.backdrop_path || movie.poster_path}` ||
+          `${BASE_URL}${movie.poster_path}`
+        }
+      />
     </div>
   );
 }
