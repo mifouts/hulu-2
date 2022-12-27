b93c4e896b4544aeed09df42999fb8f6620569c5
mifouts <fouts.mitch15@gmail.com>
2022-12-27 14:20:41 -0500

    thumbnail work

--- Components/Thumbnail.js
+++ Components/Thumbnail.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 
-function Thumbnail({ movies }) {
+function Thumbnail({ result }) {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
   return (
     <div>
@@ -11,8 +11,8 @@ function Thumbnail({ movies }) {
         width={1920}
         alt=""
         src={
-          `${BASE_URL}${movies.backdrop_path || movies.poster_path}` ||
-          `${BASE_URL}${movies.poster_path}`
+          `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
+          `${BASE_URL}${result.poster_path}`
         }
       />
     </div>
