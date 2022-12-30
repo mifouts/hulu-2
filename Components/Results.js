f943b8254da56883b9d4fd9c1eb72b9867d969a5
mifouts <fouts.mitch15@gmail.com>
2022-12-30 15:55:45 -0500

    styling

--- Components/Results.js
+++ Components/Results.js
@@ -3,7 +3,7 @@ import Thumbnail from "./Thumbnail";
 
 function Results({ results }) {
   return (
-    <div className="px-5 my-10 sm:grid md:grid-cols-2 lg:grid-cols-3">
+    <div className="px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3">
       {results.map((result) => (
         <Thumbnail key={result.id} result={result} />
       ))}
