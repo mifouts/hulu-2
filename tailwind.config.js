d7e5196dcdd4a05af65e90a4794190723fc8f30f
mifouts <fouts.mitch15@gmail.com>
2023-01-01 18:23:25 -0500

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 
-const { createGlobPatternsForDependencies } = require("@nrwl/next/tailwind");
-const { join } = require("path");
-
 module.exports = {
   mode: "jit",
   content: [
-    join(
-      __dirname,
-      "./pages/**/*.{js,ts,jsx,tsx}",
-      "./components/**/*.{js,ts,jsx,tsx}"
-    ),
-    ...createGlobPatternsForDependencies(__dirname),
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
