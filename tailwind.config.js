2a5b95beae740e0607dc875dddf7f2ae507ec579
mifouts <fouts.mitch15@gmail.com>
2023-01-01 17:33:30 -0500

    tailwind trial and error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 module.exports = {
   mode: "jit",
   content: [
-    "./pages/**/*.tsx",
-    "./components/**/*.js",
-    "./public/**/*.{html,js}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
