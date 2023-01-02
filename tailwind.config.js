e4ad4366cbd683f5d6afd206376e2e680563cac8
mifouts <fouts.mitch15@gmail.com>
2023-01-02 16:00:07 -0500

    trying everything

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      screens: {
-        "3xl": "2000px",
+enabled: process.env.NODE_ENV === "production",
+  (module.exports = {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        screens: {
+          "3xl": "2000px",
+        },
       },
     },
-  },
-  plugins: [
-    require("tailwind-scrollbar-hide"),
-    require("@tailwindcss/typography"),
-  ],
-};
+    plugins: [
+      require("tailwind-scrollbar-hide"),
+      require("@tailwindcss/typography"),
+    ],
+  });
