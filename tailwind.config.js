2d968d63a9ab8ce4c1f1037dbc122482b5f1b11f
mifouts <fouts.mitch15@gmail.com>
2023-01-01 20:07:12 -0500

    one last try

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./pages/**/*.ts", "./pages/**/*.tsx", "./components/**/*.js"],
   theme: {
     extend: {
       screens: {
